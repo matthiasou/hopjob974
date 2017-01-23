7f8aa9aa596d71c52f48fdd94d32e3ba7167dec0
bmagneCesi <benjamin.magne@viacesi.fr>
2017-01-23 16:35:53 +0100

    slider update responsive

--- src/AppBundle/Resources/public/js/global.js
+++ src/AppBundle/Resources/public/js/global.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
-	$('.bxslider').bxSlider({
-		controls:false
-	});
+
+	var width = $(window).width();
+
+	if (width <= 768) {
+		$('#bxslider-mobile').bxSlider({
+			controls:false
+		});
+	}
+
+	if (width > 768 && width < 1240) {
+		$('#bxslider-regular').bxSlider({
+			controls:false
+		});
+	}
+
+	if (width > 1240) {
+		$('#bxslider-desktop').bxSlider({
+			controls:false
+		});
+	}
+
 });
