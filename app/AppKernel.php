0b57bee32b2a40b7fa6d0509a24810940eb6a975
bmagneCesi <benjamin.magne@viacesi.fr>
2017-01-27 13:43:36 +0100

    add Admin bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Hopjob\FrontBundle\FrontBundle(),
+            new Hopjob\AdminBundle\AdminBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ src/Hopjob/AdminBundle/AdminBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Hopjob\AdminBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AdminBundle extends Bundle
+{
+}
