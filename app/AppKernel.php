13584375a9546475e19572370a2ebd09f753399c
Matthias <matthiasou@yahoo.fr>
2017-03-14 14:46:13 +0100

    admin et messagerie

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Hopjob\FrontBundle\FrontBundle(),
             new Hopjob\AdminBundle\AdminBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new FOS\MessageBundle\FOSMessageBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ src/Hopjob/AdminBundle/Entity/ThreadMetadata.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Hopjob\AdminBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\MessageBundle\Entity\ThreadMetadata as BaseThreadMetadata;
+
+/**
+ * @ORM\Entity
+ */
+class ThreadMetadata extends BaseThreadMetadata
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\ManyToOne(
+     *   targetEntity="\Hopjob\AdminBundle\Entity\Thread",
+     *   inversedBy="metadata"
+     * )
+     * @var \FOS\MessageBundle\Model\ThreadInterface
+     */
+    protected $thread;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="\Hopjob\FrontBundle\Entity\User")
+     * @var \FOS\MessageBundle\Model\ParticipantInterface
+     */
+    protected $participant;
+}
