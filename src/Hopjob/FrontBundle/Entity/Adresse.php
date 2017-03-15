e0f68caf733c137e66fe61eb76aa83742f609919
bmagneCesi <benjamin.magne@viacesi.fr>
2017-03-15 14:56:59 +0100

    Mise a jour entité Ville

---
+++ src/Hopjob/FrontBundle/Entity/Adresse.php
@@ -0,0 +1,167 @@
+<?php
+
+namespace Hopjob\FrontBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Adresse
+ *
+ * @ORM\Table(name="adresse")
+ * @ORM\Entity(repositoryClass="Hopjob\FrontBundle\Repository\AdresseRepository")
+ */
+class Adresse
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\id
+     * @ORM\OneToMany(targetEntity="Hopjob\FrontBundle\entity\User", mappedBy="adresse")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="utilisateur", type="string", length=255)
+     */
+    private $utilisateur;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="adresse1", type="string", length=255)
+     */
+    private $adresse1;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="adresse2", type="string", length=255, nullable=true)
+     */
+    private $adresse2;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="codePostal", type="string", length=255)
+     */
+    private $codePostal;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="ville", type="string", length=255)
+     * @ORM\OneToOne(targetEntity="Hopjob\FrontBundle\entity\Ville")
+     */
+    private $ville;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set adresse1
+     *
+     * @param string $adresse1
+     *
+     * @return Adresse
+     */
+    public function setAdresse1($adresse1)
+    {
+        $this->adresse1 = $adresse1;
+
+        return $this;
+    }
+
+    /**
+     * Get adresse1
+     *
+     * @return string
+     */
+    public function getAdresse1()
+    {
+        return $this->adresse1;
+    }
+
+    /**
+     * Set adresse2
+     *
+     * @param string $adresse2
+     *
+     * @return Adresse
+     */
+    public function setAdresse2($adresse2)
+    {
+        $this->adresse2 = $adresse2;
+
+        return $this;
+    }
+
+    /**
+     * Get adresse2
+     *
+     * @return string
+     */
+    public function getAdresse2()
+    {
+        return $this->adresse2;
+    }
+
+    /**
+     * Set codePostal
+     *
+     * @param string $codePostal
+     *
+     * @return Adresse
+     */
+    public function setCodePostal($codePostal)
+    {
+        $this->codePostal = $codePostal;
+
+        return $this;
+    }
+
+    /**
+     * Get codePostal
+     *
+     * @return string
+     */
+    public function getCodePostal()
+    {
+        return $this->codePostal;
+    }
+
+    /**
+     * Set ville
+     *
+     * @param string $ville
+     *
+     * @return Adresse
+     */
+    public function setVille($ville)
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    /**
+     * Get ville
+     *
+     * @return string
+     */
+    public function getVille()
+    {
+        return $this->ville;
+    }
+
+}
--- src/Hopjob/FrontBundle/Entity/Ville.php
+++ src/Hopjob/FrontBundle/Entity/Ville.php
@@ -19,148 +19,42 @@ class Ville
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      * @ORM\OneToMany(targetEntity="Hopjob\FrontBundle\Entity\Annonce", mappedBy="ville")
-     * @ORM\OneToMany(targetEntity="Hopjob\FrontBundle\entity\User", mappedBy="ville")
      */
     private $id;
 
     /**
      * @var string
      *
-     * @ORM\Column(name="departement", type="string", length=3)
+     * @ORM\Column(name="libelle", type="string", length=255)
      */
-    private $departement;
+    private $libelle;
 
     /**
      * @var string
      *
-     * @ORM\Column(name="slug", type="string", length=255)
-     */
-    private $slug;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="nom", type="string", length=255)
-     */
-    private $nom;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="nom_simple", type="string", length=255)
-     */
-    private $nomSimple;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="nom_reel", type="string", length=255)
-     */
-    private $nomReel;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="nom_soundex", type="string", length=255)
-     */
-    private $nomSoundex;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="code_postal", type="string", length=255)
+     * @ORM\Column(name="code_postal", type="string", length=5)
      */
     private $codePostal;
 
     /**
      * @var string
      *
-     * @ORM\Column(name="commune", type="string", length=255)
+     * @ORM\Column(name="latitude", type="string", length=255)
      */
-    private $commune;
+    private $latitude;
 
     /**
      * @var string
      *
-     * @ORM\Column(name="code_commune", type="string", length=255)
+     * @ORM\Column(name="longitude", type="string", length=255)
      */
-    private $codeCommune;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="arrondissement", type="string", length=255)
-     */
-    private $arrondissement;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="canton", type="string", length=255)
-     */
-    private $canton;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="amdi", type="string", length=255)
-     */
-    private $amdi;
-
-    /**
-     * @var float
-     *
-     * @ORM\Column(name="longitude_deg", type="float")
-     */
-    private $longitudeDeg;
-
-    /**
-     * @var float
-     *
-     * @ORM\Column(name="latitude_deg", type="float")
-     */
-    private $latitudeDeg;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="longitude_grd", type="string", length=255)
-     */
-    private $longitudeGrd;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="latitude_grd", type="string", length=255)
-     */
-    private $latitudeGrd;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="longitude_dms", type="string", length=255)
-     */
-    private $longitudeDms;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="latitude_dms", type="string", length=255)
-     */
-    private $latitudeDms;
-
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="zmin", type="integer")
-     */
-    private $zmin;
+    private $longitude;
 
 
     /**
      * Get id
      *
-     * @return int
+     * @return integer
      */
     public function getId()
     {
@@ -168,147 +62,27 @@ class Ville
     }
 
     /**
-     * Set departement
+     * Set libelle
      *
-     * @param string $departement
+     * @param string $libelle
      *
      * @return Ville
      */
-    public function setDepartement($departement)
+    public function setLibelle($libelle)
     {
-        $this->departement = $departement;
+        $this->libelle = $libelle;
 
         return $this;
     }
 
     /**
-     * Get departement
+     * Get libelle
      *
      * @return string
      */
-    public function getDepartement()
+    public function getLibelle()
     {
-        return $this->departement;
-    }
-
-    /**
-     * Set slug
-     *
-     * @param string $slug
-     *
-     * @return Ville
-     */
-    public function setSlug($slug)
-    {
-        $this->slug = $slug;
-
-        return $this;
-    }
-
-    /**
-     * Get slug
-     *
-     * @return string
-     */
-    public function getSlug()
-    {
-        return $this->slug;
-    }
-
-    /**
-     * Set nom
-     *
-     * @param string $nom
-     *
-     * @return Ville
-     */
-    public function setNom($nom)
-    {
-        $this->nom = $nom;
-
-        return $this;
-    }
-
-    /**
-     * Get nom
-     *
-     * @return string
-     */
-    public function getNom()
-    {
-        return $this->nom;
-    }
-
-    /**
-     * Set nomSimple
-     *
-     * @param string $nomSimple
-     *
-     * @return Ville
-     */
-    public function setNomSimple($nomSimple)
-    {
-        $this->nomSimple = $nomSimple;
-
-        return $this;
-    }
-
-    /**
-     * Get nomSimple
-     *
-     * @return string
-     */
-    public function getNomSimple()
-    {
-        return $this->nomSimple;
-    }
-
-    /**
-     * Set nomReel
-     *
-     * @param string $nomReel
-     *
-     * @return Ville
-     */
-    public function setNomReel($nomReel)
-    {
-        $this->nomReel = $nomReel;
-
-        return $this;
-    }
-
-    /**
-     * Get nomReel
-     *
-     * @return string
-     */
-    public function getNomReel()
-    {
-        return $this->nomReel;
-    }
-
-    /**
-     * Set nomSoundex
-     *
-     * @param string $nomSoundex
-     *
-     * @return Ville
-     */
-    public function setNomSoundex($nomSoundex)
-    {
-        $this->nomSoundex = $nomSoundex;
-
-        return $this;
-    }
-
-    /**
-     * Get nomSoundex
-     *
-     * @return string
-     */
-    public function getNomSoundex()
-    {
-        return $this->nomSoundex;
+        return $this->libelle;
     }
 
     /**
@@ -336,295 +110,50 @@ class Ville
     }
 
     /**
-     * Set commune
+     * Set latitude
      *
-     * @param string $commune
+     * @param string $latitude
      *
      * @return Ville
      */
-    public function setCommune($commune)
+    public function setLatitude($latitude)
     {
-        $this->commune = $commune;
+        $this->latitude = $latitude;
 
         return $this;
     }
 
     /**
-     * Get commune
+     * Get latitude
      *
      * @return string
      */
-    public function getCommune()
+    public function getLatitude()
     {
-        return $this->commune;
+        return $this->latitude;
     }
 
     /**
-     * Set codeCommune
+     * Set longitude
      *
-     * @param string $codeCommune
+     * @param string $longitude
      *
      * @return Ville
      */
-    public function setCodeCommune($codeCommune)
+    public function setLongitude($longitude)
     {
-        $this->codeCommune = $codeCommune;
+        $this->longitude = $longitude;
 
         return $this;
     }
 
     /**
-     * Get codeCommune
+     * Get longitude
      *
      * @return string
      */
-    public function getCodeCommune()
+    public function getLongitude()
     {
-        return $this->codeCommune;
-    }
-
-    /**
-     * Set arrondissement
-     *
-     * @param string $arrondissement
-     *
-     * @return Ville
-     */
-    public function setArrondissement($arrondissement)
-    {
-        $this->arrondissement = $arrondissement;
-
-        return $this;
-    }
-
-    /**
-     * Get arrondissement
-     *
-     * @return string
-     */
-    public function getArrondissement()
-    {
-        return $this->arrondissement;
-    }
-
-    /**
-     * Set canton
-     *
-     * @param string $canton
-     *
-     * @return Ville
-     */
-    public function setCanton($canton)
-    {
-        $this->canton = $canton;
-
-        return $this;
-    }
-
-    /**
-     * Get canton
-     *
-     * @return string
-     */
-    public function getCanton()
-    {
-        return $this->canton;
-    }
-
-    /**
-     * Set amdi
-     *
-     * @param string $amdi
-     *
-     * @return Ville
-     */
-    public function setAmdi($amdi)
-    {
-        $this->amdi = $amdi;
-
-        return $this;
-    }
-
-    /**
-     * Get amdi
-     *
-     * @return string
-     */
-    public function getAmdi()
-    {
-        return $this->amdi;
-    }
-
-    /**
-     * Set longitudeDeg
-     *
-     * @param float $longitudeDeg
-     *
-     * @return Ville
-     */
-    public function setLongitudeDeg($longitudeDeg)
-    {
-        $this->longitudeDeg = $longitudeDeg;
-
-        return $this;
-    }
-
-    /**
-     * Get longitudeDeg
-     *
-     * @return float
-     */
-    public function getLongitudeDeg()
-    {
-        return $this->longitudeDeg;
-    }
-
-    /**
-     * Set latitudeDeg
-     *
-     * @param float $latitudeDeg
-     *
-     * @return Ville
-     */
-    public function setLatitudeDeg($latitudeDeg)
-    {
-        $this->latitudeDeg = $latitudeDeg;
-
-        return $this;
-    }
-
-    /**
-     * Get latitudeDeg
-     *
-     * @return float
-     */
-    public function getLatitudeDeg()
-    {
-        return $this->latitudeDeg;
-    }
-
-    /**
-     * Set longitudeGrd
-     *
-     * @param string $longitudeGrd
-     *
-     * @return Ville
-     */
-    public function setLongitudeGrd($longitudeGrd)
-    {
-        $this->longitudeGrd = $longitudeGrd;
-
-        return $this;
-    }
-
-    /**
-     * Get longitudeGrd
-     *
-     * @return string
-     */
-    public function getLongitudeGrd()
-    {
-        return $this->longitudeGrd;
-    }
-
-    /**
-     * Set latitudeGrd
-     *
-     * @param string $latitudeGrd
-     *
-     * @return Ville
-     */
-    public function setLatitudeGrd($latitudeGrd)
-    {
-        $this->latitudeGrd = $latitudeGrd;
-
-        return $this;
-    }
-
-    /**
-     * Get latitudeGrd
-     *
-     * @return string
-     */
-    public function getLatitudeGrd()
-    {
-        return $this->latitudeGrd;
-    }
-
-    /**
-     * Set longitudeDms
-     *
-     * @param string $longitudeDms
-     *
-     * @return Ville
-     */
-    public function setLongitudeDms($longitudeDms)
-    {
-        $this->longitudeDms = $longitudeDms;
-
-        return $this;
-    }
-
-    /**
-     * Get longitudeDms
-     *
-     * @return string
-     */
-    public function getLongitudeDms()
-    {
-        return $this->longitudeDms;
-    }
-
-    /**
-     * Set latitudeDms
-     *
-     * @param string $latitudeDms
-     *
-     * @return Ville
-     */
-    public function setLatitudeDms($latitudeDms)
-    {
-        $this->latitudeDms = $latitudeDms;
-
-        return $this;
-    }
-
-    /**
-     * Get latitudeDms
-     *
-     * @return string
-     */
-    public function getLatitudeDms()
-    {
-        return $this->latitudeDms;
-    }
-
-    /**
-     * Set zmin
-     *
-     * @param integer $zmin
-     *
-     * @return Ville
-     */
-    public function setZmin($zmin)
-    {
-        $this->zmin = $zmin;
-
-        return $this;
-    }
-
-    /**
-     * Get zmin
-     *
-     * @return int
-     */
-    public function getZmin()
-    {
-        return $this->zmin;
-    }
-
-    public function __toString()
-    {
-        return $this->nom;
+        return $this->longitude;
     }
 }
