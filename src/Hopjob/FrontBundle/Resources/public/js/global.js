b29ed28834d886c823b068ed78f7facffdf945b9
bmagneCesi <bmagne@bobbies.fr>
2017-06-02 15:29:55 +0200

    creation du profil et modification

--- src/Hopjob/FrontBundle/Resources/public/js/global.js
+++ src/Hopjob/FrontBundle/Resources/public/js/global.js
@@ -6,10 +6,37 @@ $(document).ready(function(){
 
 // ENDHOMEPAGE
 
-$('.hide-black-background').click(function(){
-	$('.black-background').fadeOut();
-	$('.modal-register-success').fadeOut();
-});
+	$('.hide-black-background').click(function(){
+		$('.black-background').fadeOut();
+		$('.modal-register-success').fadeOut();
+	});
+
+// NAV
+
+    var showHeaderAt = 150;
+
+    var win = $(window),
+            body = $('body');
+
+    // Show the fixed header only on larger screen devices
+
+    if(win.width() > 600){
+
+        // When we scroll more than 150px down, we set the
+        // "fixed" class on the body element.
+
+        win.on('scroll', function(e){
+
+            if(win.scrollTop() > showHeaderAt) {
+                body.addClass('fixed');
+            }
+            else {
+                body.removeClass('fixed');
+            }
+        });
+
+    }
+// FIN NAV
 
 });
 
